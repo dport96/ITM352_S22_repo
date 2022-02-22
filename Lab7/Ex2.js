aa989a99d4430b662c0163d5b9f58a4edd058a1d
D. Port Me <dport@hawaii.edu>
2022-02-22 13:22:03 -1000

    xxx

---
+++ Lab7/Ex2.js
@@ -0,0 +1,9 @@
+require("./product_data.js");
+
+
+for(product_index = 1; eval("typeof name"+product_index) != 'undefined' ; product_index++) {
+
+    console.log(`${product_index}. ${eval('name' + product_index)}`);
+    
+}
+console.log("That's all we have!");
---
+++ Lab7/product_data.js
@@ -0,0 +1,19 @@
+name1 = "HTC";
+price1 = 40.00;
+image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+
+name2 = "Apple";
+price2 = 75.00;
+image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+name3 = "Nokia";
+price3 = 35.00;
+image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+
+name4 = "Samsung";
+price4 = 45.00;
+image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+
+
+
+console.log("Product variables loaded...");
