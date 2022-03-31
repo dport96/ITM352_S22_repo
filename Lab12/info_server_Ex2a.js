85d6c3ab57023c0ed35ad8d727227fd118b41783
D. Port Me <dport@hawaii.edu>
2022-03-31 13:25:29 -1000

    xxx

--- Lab12/info_server_Ex2a.js
+++ Lab12/info_server_Ex2a.js
@@ -15,11 +15,22 @@ app.get('/test', function (request, response, next) {
 });
 
 app.post('/process_form', function (request, response, next) {
+    var errors = {};
+
     var q = request.body['qty_textbox1'];
-    if (typeof q != 'undefined') {
-    response.send(`Thank you for purchasing ${q} things!`);
+    // invalaid if q is not a quanity
+    if (!isNonNegInt(q)) {
+       errors['qantity'] = isNonNegInt(q, true);
+    } 
+    // check is some items were selected
+    if(q == 0) {
+        errors['no_qanitity'] = `You need to select some things`;
+    }
+
+    if( Object.keys(errors).length == 0 ) {
+        response.send(`Thank you for purchasing ${q} things!`);
     } else {
-        response.send(`Nothing selected`);
+        response.redirect('./order_page.html?errors=' + JSON.stringify(errors) );
     }
 });
 
@@ -27,3 +38,15 @@ app.post('/process_form', function (request, response, next) {
 app.use(express.static(__dirname + '/public'));
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+
+
+function isNonNegInt(q, returnErrors = false) {
+    errors = []; // assume no errors at first
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    else {
+        if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+        if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    }
+
+    return (returnErrors ? errors : (errors.length == 0));
+}
